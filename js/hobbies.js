e4e4cfac6de663195d04040cd7b0370240138532
Ylenia <yleniamighela@gmail.com>
2025-01-31 15:50:56 +0100

    added function and export file names.js

--- js/hobbies.js
+++ js/hobbies.js
@@ -1,2 +1,3 @@
 // 1. Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire un array di stringhe contenenti gli hobby.
 // 2. Esporta la funzione dal file.
+console.log("ciao");
--- js/names.js
+++ js/names.js
@@ -1,3 +1,25 @@
 // 1 - names.js
 // 1. Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
 // 2. Esporta la funzione dal file.
+
+
+
+// creo una funzione in cui mi restituirà un oggetto con due proprietà
+function fullName(firstName, lastName){
+
+    return {
+
+        firstName : firstName,
+        lastName : lastName
+    };
+
+
+};
+
+// console.log(fullName("ylenia","mighela"));
+
+
+
+
+// ESPORTO LA FUNZIONE
+module.exports = fullName;
