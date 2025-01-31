3261b4358a98ae46a9de5f7d75cf52915a2b3b31
Ylenia <yleniamighela@gmail.com>
2025-01-31 16:07:03 +0100

    added import function fullName and hobbies

--- js/people.js
+++ js/people.js
@@ -2,4 +2,13 @@
 // 2. Importa la tua funzione da hobbies.js
 // 3. Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. All’interno della tua funzione, usa le tue due funzioni precedenti per costruire l’oggetto.
 
-console.log("ciao");
+
+
+
+// importo la funzione fullName
+const fullName = require ("./names.js");
+
+
+
+// importo la funzione hobbies
+const hobbies = require("./hobbies.js");
